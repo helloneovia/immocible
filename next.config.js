78746252e90203ca20d50ea1c7ba675450f38545
eurlcernunnos-star <eurlcernunnos@gmail.com>
2026-01-20 11:23:07 +0100

    Fix: Forcer mise a jour de tous les fichiers essentiels

---
+++ next.config.js
@@ -0,0 +1,18 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    typedRoutes: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+}
+
+module.exports = nextConfig
